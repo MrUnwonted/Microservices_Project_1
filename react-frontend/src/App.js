cbdf50727d74053bc5c40ca0125f74d99f0a7d89
MrUnwonted <arjungodzarjun@gmail.com>
2023-09-20 19:34:27 +0530

    React Added

---
+++ react-frontend/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import EmployeeComponent from './Component/EmployeeComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       
+      <EmployeeComponent />
+      </header>
+    </div>
+  );
+}
+
+export default App;

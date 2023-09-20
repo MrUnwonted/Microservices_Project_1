634e9937d9cb2d39607c389ec236e0d706314570
MrUnwonted <arjungodzarjun@gmail.com>
2023-09-20 20:42:28 +0530

    Migrated to 3.0

--- employee-service/src/main/java/com/arjun/employeeservice/dto/OrganizationDto.java
+++ employee-service/src/main/java/com/arjun/employeeservice/dto/OrganizationDto.java
@@ -6,7 +6,6 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 
-import javax.persistence.Column;
 import java.time.LocalDateTime;
 
 @Setter

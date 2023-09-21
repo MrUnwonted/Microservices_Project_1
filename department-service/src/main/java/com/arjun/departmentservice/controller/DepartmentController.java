dbc553b1820c91be4a3c790d61d0a43f9810f5a2
MrUnwonted <arjungodzarjun@gmail.com>
2023-09-21 14:35:16 +0530

    Swagger Config Dep

--- department-service/src/main/java/com/arjun/departmentservice/controller/DepartmentController.java
+++ department-service/src/main/java/com/arjun/departmentservice/controller/DepartmentController.java
@@ -2,10 +2,19 @@ package com.arjun.departmentservice.controller;
 
 import com.arjun.departmentservice.dto.DepartmentDto;
 import com.arjun.departmentservice.service.DepartmentService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+@Tag(
+        name = "Department Service - DepartmentController",
+        description = "Department Controller Exposes REST APIs for Department-Service"
+)
+
 
 @RestController
 @RequestMapping("api/departments")
@@ -14,12 +23,30 @@ public class DepartmentController {
 
     private DepartmentService departmentService;
 
+    @Operation(
+            summary = "Save Department REST API",
+            description = "Save Department REST API is used to save department object in a database"
+    )
+    @ApiResponse(
+            responseCode = "201",
+            description = "HTTP Status 201 CREATED"
+    )
+
     // Build save department REST API
     @PostMapping
     public ResponseEntity<DepartmentDto> saveDepartment(@RequestBody DepartmentDto departmentDto){
         DepartmentDto savedDepartment = departmentService.saveDepartment(departmentDto);
         return new ResponseEntity<>(savedDepartment, HttpStatus.CREATED);
     }
+
+    @Operation(
+            summary = "Get Department REST API",
+            description = "Get Department REST API is used to get department object from the database"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "HTTP Status 200 SUCCESS"
+    )
 
     // Build get department rest api
     @GetMapping("{department-code}")
--- department-service/src/main/java/com/arjun/departmentservice/dto/DepartmentDto.java
+++ department-service/src/main/java/com/arjun/departmentservice/dto/DepartmentDto.java
@@ -1,9 +1,14 @@
 package com.arjun.departmentservice.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+@Schema(
+        description = "DepartmentDTO Model Information"
+)
 
 @Setter
 @Getter
@@ -11,7 +16,16 @@ import lombok.Setter;
 @AllArgsConstructor
 public class DepartmentDto {
     private Long id;
+    @Schema(
+            description = "Department Name"
+    )
     private String departmentName;
+    @Schema(
+            description = "Department Description"
+    )
     private String departmentDescription;
+    @Schema(
+            description = "Department Code"
+    )
     private String departmentCode;
 }
